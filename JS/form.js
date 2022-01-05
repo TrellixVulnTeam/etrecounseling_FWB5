fe2422c406c26cebc3459c20ecbc061045c40e10
Juan Manuel Roig Courtis <jmroigcourtis.dev@gmail.com>
2022-01-05 17:13:29 -0300

    Timer y storage agregados al form

--- JS/form.js
+++ JS/form.js
@@ -3,8 +3,31 @@ const EnviarFormulario = () => {
     Boton.on("click", function(e){
         e.preventDefault
         const Nombre = $("#Name").val()
-        $("#Mensaje").html(`<h2>¡Hola ${Nombre}, tu mensaje fue enviado con exito!<h2>`);
+        $("#Mensaje").append(`<h2 class="d-flex justify-content-center animate__animated animate__fadeIn" >¡Hola ${Nombre}, tu mensaje fue enviado con exito!<h2>`);
+        setTimeout(function(){
+            window.location.replace(`index.html`);
+        }, 3000);
+    
     });
 }
 
+const formStorage = () => {
+    [
+        {
+    "User" : 
+        sessionStorage.setItem("User", $("#Name").val())
+        },
+
+        {
+    "Email" : 
+        sessionStorage.setItem("Email", $("#Email").val())
+        }
+    ]
+
+}
+
+
 EnviarFormulario ();
+formStorage();
+
+
